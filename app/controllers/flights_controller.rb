7c8fda84369f40f5651072304d4acdc0af524c10
richie <rsaldiva@gmail.com>
2015-10-27 22:07:34 -0400

    update get flight query

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,5 +6,15 @@ class FlightsController < ApplicationController
     @airport_options = Airport.all.map{|u| [ u.code, u.id ] }
     @dates = Flight.get_dates
     @flights = Flight.all
-  end
+    
+    if params[:start] != nil && params[:from_airport_id] != nil && params[:to_airport_id]
+    @searching = true
+      if params[:from_airport_id] == params[:to_airport_id]
+				flash.now[:error] = "Your 'From' and 'To' airports can't be the same"
+			else
+				@matched_flights = @flights.where("from_airport_id = ? AND to_airport_id = ? AND strftime('%Y-%m-%d', start) = ?", params[:from_airport_id], params[:to_airport_id], params[:start])
+       
+			end
+    end
+    end
 end
